b6617d3f52312f97b3474bd4c159d5615d80bd27
kal <46622982+heekal@users.noreply.github.com>
2026-06-17 10:05:59 +0700

    Add files via upload

---
+++ run_all.py
@@ -0,0 +1,22 @@
+import subprocess
+
+commands = [
+  "python etl_pipeline/01_extract_etl.py",
+  "python etl_pipeline/02_transform_etl.py",
+  "python etl_pipeline/03_load_etl.py",
+  "python elt_pipeline/01_extract_load_elt.py",
+  "python elt_pipeline/03_run_elt_transform.py",
+  "python create_dashboard_table.py"
+]
+
+for command in commands:
+  print("\nRunning:", command)
+
+  result = subprocess.run(command, shell=True)
+
+  if result.returncode != 0:
+    raise RuntimeError(f"Command failed: {command}")
+
+print("\nFinish!")
+print("All pipeline ran successfully")
+print("dashboard_trip is ready for Power BI refresh")
